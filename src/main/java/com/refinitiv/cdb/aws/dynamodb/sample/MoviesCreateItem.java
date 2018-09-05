1f863ed89008b6811087b675064d91495332b321
Jing Wang <jing.wang@thomsonreuters.com>
2018-09-05 13:11:27 +0800

    no message

---
+++ src/main/java/com/refinitiv/cdb/aws/dynamodb/sample/MoviesCreateItem.java
@@ -0,0 +1,54 @@
+package com.refinitiv.cdb.aws.dynamodb.sample;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.document.DynamoDB;
+import com.amazonaws.services.dynamodbv2.document.Item;
+import com.amazonaws.services.dynamodbv2.document.PutItemOutcome;
+import com.amazonaws.services.dynamodbv2.document.Table;
+
+public class MoviesCreateItem {
+
+    public static void main(String[] args) throws Exception {
+
+    		/*
+        AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard()
+            .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration("http://localhost:8000", "us-west-2"))
+            .build();
+            */
+    	
+		AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard()
+    			.withRegion(Regions.US_EAST_1)
+    			.build();
+
+        DynamoDB dynamoDB = new DynamoDB(client);
+
+        Table table = dynamoDB.getTable("Movies");
+
+        int year = 2015;
+        String title = "The Big New Movie";
+
+        final Map<String, Object> infoMap = new HashMap<String, Object>();
+        infoMap.put("plot", "Nothing happens at all.");
+        infoMap.put("rating", 0);
+
+        try {
+            System.out.println("Adding a new item...");
+            PutItemOutcome outcome = table
+                .putItem(new Item().withPrimaryKey("year", year, "title", title).withMap("info", infoMap));
+
+            System.out.println("PutItem succeeded:\n" + outcome.getPutItemResult());
+
+        }
+        catch (Exception e) {
+            System.err.println("Unable to add item: " + year + " " + title);
+            System.err.println(e.getMessage());
+        }
+
+    }
+}
